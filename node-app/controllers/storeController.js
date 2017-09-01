76b58d5d7bdbe0b1f50ed36e52e2ef522927af2d
David Saunders <david@monkee-boy.com>
2017-09-01 10:34:37 -0500

    Pull store content into mixin and display in store.pug template

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -16,3 +16,9 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review`); // makes use of flash middleware
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async(req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find(); // Returns promise
+  res.render('stores', { title: 'Stores', stores }); // pass the data to the template
+}
--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -4,7 +4,8 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers'); // Object destructuring -- allows us to import an entire object
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore)); // Immediately runs catchErrors on post submit -- pushes errors in later middleware. Lets us avoid handling errors in the async/await function itself.
 
