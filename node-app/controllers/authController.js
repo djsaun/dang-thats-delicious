854cc0316c2189a90b6b9ac9b3a6b8f7b62a63a9
David Saunders <david@monkee-boy.com>
2017-09-08 11:23:43 -0500

    Created isLoggedIn method; Ensure that user is logged into the site before they can create a store

--- node-app/controllers/authController.js
+++ node-app/controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {  // checks with passport if user is there
+    next(); // continue -- they are logged in
+    return;
+  }
+  req.flash('error', 'Oops! You must be logged in to do that!');
+  res.redirect('/login');
+}
