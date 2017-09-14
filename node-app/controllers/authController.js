8d20071471b25fc7ec28e90040bc72c91a518e77
David Saunders <david@monkee-boy.com>
2017-09-14 09:27:46 -0500

    Generate password reset URL

--- node-app/controllers/authController.js
+++ node-app/controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto'); // native node module
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', { // pass in strategy (local is for username/pw) and config object
  failureRedirect: '/login', // if failure, where should they go?
@@ -20,5 +23,26 @@ exports.isLoggedIn = (req, res, next) => {
     return;
   }
   req.flash('error', 'Oops! You must be logged in to do that!');
+  res.redirect('/login');
+}
+
+exports.forgot = async (req, res) => {
+  // 1. Check if user with that email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No account with that email exists.');
+    return res.redirect('/login');
+  }
+
+  // 2. Set reset tokens and expiry on account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+
+  // 3. Send them an email with the token
+  const resetUrl = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetUrl}`);
+
+  // 4. Redirect to login page after redirect token has been sent
   res.redirect('/login');
 }
